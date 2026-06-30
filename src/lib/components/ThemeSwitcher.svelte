47006a3c578b3c17e40d752eca60c6deb365251b
fantome <a.lyaskovsky@gmail.com>
2026-06-30 12:41:34 +0300

    fix theme switcher initial value

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -1,31 +1,17 @@
 <script lang="ts">
 	import { i18n } from '$lib/i18n/state.svelte';
-	import { getPreferredTheme, setTheme, type Theme } from '$lib/theme';
+	import { setTheme } from '$lib/theme';
 	import { MoonIcon, SunIcon } from '@lucide/svelte';
 	import Button from './Button.svelte';
 	const title = i18n.messages.pages.room.settings.theme;
-	let theme: Theme = $state('light');
-
-	$effect(() => {
-		theme = getPreferredTheme();
-		setTheme(theme);
-	});
 
 	function toggleTheme() {
-		theme = theme === 'light' ? 'dark' : 'light';
-		setTheme(theme);
+		const attr = document.documentElement.getAttribute('data-theme');
+		setTheme(attr === 'dark' ? 'light' : 'dark');
 	}
 </script>
 
 <Button variant="ghost" class="btn relative" onclick={toggleTheme} {title}>
-	<SunIcon
-		class=" transition-all duration-300 {theme === 'dark'
-			? 'hidden scale-0 -rotate-90'
-			: 'scale-100 rotate-0'}"
-	/>
-	<MoonIcon
-		class=" transition-all duration-300 {theme === 'dark'
-			? 'scale-100 rotate-0'
-			: 'hidden scale-0 rotate-90'}"
-	/>
+	<SunIcon class="transition-all duration-300 icon-sun" />
+	<MoonIcon class="transition-all duration-300 icon-moon" />
 </Button>
